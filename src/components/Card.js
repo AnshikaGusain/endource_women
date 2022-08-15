c94b0a3725accf9af6e30a54ecf12f6d809b9d05
Anshika Gusain <anshikagusain62@gmail.com>
2022-08-15 22:24:51 +0530

    editing cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,10 @@ const Card = ({ title, img,link}) => {
         <div className="col-6 col-sm-4 col-md-3">
             <Link to={`/${link}/${title.toLowerCase()}`}>
             <div className="card">
-                <img src={img} style={{padding:"15px" ,width:"100%",
-    maxWidth:"300px",maxHeight:"300px",minHeight:"200px", position:"relative",border:"none" }} className="card-img-top" alt="..." />
+                <div className="justify-content-center">
+                    
+                <img src={img} style={{padding:"15px" ,width:"100%", maxWidth:"300px",maxHeight:"300px",minHeight:"200px",position:"relative",border:"none" }} className="card-img-top" alt="..." />
+                </div>
                 <div className="card-body">
                     <span className="btn btn-outline-dark"><h3 className="categoryTitle">{title}</h3></span>
                 </div>
