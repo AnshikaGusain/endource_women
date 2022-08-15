fe7a2dd6fc7ce583f93e73a22e455fb9f7a64620
Anshika Gusain <anshikagusain62@gmail.com>
2022-08-15 21:27:23 +0530

    Changing product page

--- src/components/Prod.js
+++ src/components/Prod.js
@@ -3,11 +3,11 @@ const Prod =({prod})=>{
         <div className="product container-fluid">
             <div className="d-flex flex-wrap justify-content-around container-fluid">
                 <div className="img bg-light" style={{width:"25rem", height:"30rem"}} ><img src={prod.img} width="100%" height="100%" className="p-15" alt="Fail to load"/></div>
-                <div>
+                <div className="text-center">
                     <div className="title p-3 m-4 w-100 text-decoration-underline"><h1>{prod.title}</h1></div>
-                    <div className="brand p-1 m-2 w-100"><h3>Brand: {prod.brand}</h3></div>
-                    <div className="price p-1 m-2 w-100"><h3>Price: {prod.price}</h3></div>
-                    <button className="btn btn-outline-info"><h4>Buy from {prod.brand}</h4></button>
+                    <div className="brand p-1 m-2 w-100 text-center"><h3>Brand: {prod.brand}</h3></div>
+                    <div className="price p-1 m-2 w-100 text-center"><h3>Price: {prod.price}</h3></div>
+                    <button className="btn btn-outline-dark"><h4>Buy</h4></button>
                     </div>
             </div>
             <h2 className="text-decoration-underline m-4">Description</h2>
