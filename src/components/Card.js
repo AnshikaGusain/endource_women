ce818b3a9c3d5f14023b21ec43e4e9624ed11ed3
Anshika Gusain <anshikagusain62@gmail.com>
2022-08-16 14:38:10 +0530

    making card height in terms of vw

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,10 @@ const Card = ({ title, img,link}) => {
     return (
         <div className="col-6 col-sm-4 col-md-3">
             <Link to={`/${link}/${title.toLowerCase()}`}>
-            <div className="card">
+            <div className="card" >
                 <div className="justify-content-center">
                     
-                <img className="img-fluid card-img-top" src={img} style={{padding:"15px" ,width:"100%", maxWidth:"300px",maxHeight:"300px",minHeight:"200px",position:"relative",border:"none" }}  alt="..." />
+                <img className="img-fluid card-img-top" src={img} style={{padding:"15px" ,width:"100%", maxWidth:"300px",maxHeight:"300px",position:"relative",border:"none" }}  alt="..." />
                 </div>
                 <div className="card-body">
                     <span className="btn btn-outline-dark"><h3 className="categoryTitle">{title}</h3></span>
